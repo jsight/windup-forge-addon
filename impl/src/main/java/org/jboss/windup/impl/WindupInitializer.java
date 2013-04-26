e62b3f45879a632e7e92a9a5edf5cffa93a8902d
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2013-04-26 16:05:15 -0400

    Initial revision

---
+++ impl/src/main/java/org/jboss/windup/impl/WindupInitializer.java
@@ -0,0 +1,20 @@
+package org.jboss.windup.impl;
+
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+
+import org.jboss.forge.container.Forge;
+import org.jboss.forge.container.event.PostStartup;
+
+/**
+ * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ */
+public class WindupInitializer {
+
+	@Inject
+	private Forge forge;
+
+	public void run(@Observes PostStartup event) {
+		WindupMain.main(forge.getArgs());
+	}
+}
